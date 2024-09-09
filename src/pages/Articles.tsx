ef2a2e7c1c11a9eb66e4077f4a895561c5972341
Evgeniy2022 <t0lrk0ln22rus@mail.ru>
2024-09-09 19:08:46 +0300

    fix next bag

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react";
 import { ArticlePsot } from "../components/ArticlePsot/ArticlePsot";
-import { Pagination } from '../components/Pagination/Pagination';
-import { AticleType } from '../articleType';
+import { Pagination } from "../components/Pagination/Pagination";
+import { AticleType } from "../articleType";
 
 interface ArticlesProps {
   articles: AticleType[];
@@ -9,11 +9,18 @@ interface ArticlesProps {
   setPage: (arg: number) => void;
   pagesAll: number;
 }
-const storageItem = JSON.parse(localStorage.getItem("fovoriteSlugs") || "");
+const storageItem = localStorage.getItem("fovoriteSlugs")
+  ? JSON.parse(localStorage.getItem("fovoriteSlugs") || "")
+  : "";
 
-export const Articles: FC<ArticlesProps> = ({ articles, page, setPage, pagesAll }) => {
+export const Articles: FC<ArticlesProps> = ({
+  articles,
+  page,
+  setPage,
+  pagesAll,
+}) => {
   const [saveSlugs, setSaveSlugs] = useState(storageItem);
-	
+
   return (
     <>
       {articles.map((article, index) => {
